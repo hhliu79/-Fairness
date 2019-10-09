883356a366a749515b25179e0eb2b38f41748314
hhliu79 <55559937+hhliu79@users.noreply.github.com>
2019-10-09 01:45:09 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
@@ -8,6 +9,9 @@ Created on Thu Oct  3 10:37:03 2019
 
 # Load all necessary packages
 import sys
+import copy
+import numpy as np
+
 sys.path.append("../")
 from aif360.metrics import BinaryLabelDatasetMetric
 from loaddata import LoadData
@@ -28,14 +32,16 @@ from aif360.metrics import ClassificationMetric
 from common_utils import compute_metrics
 from collections import OrderedDict
 import os
-os.environ['CUDA_VISIBLE_DEVICES']='8'
+#os.environ['CUDA_VISIBLE_DEVICES']='7'
 
-dataset = ["german", "adult", "compas"]  
+#dataset = ["adult", "compas", "german", "bank"]  
+dataset = ["adult", "compas"]  
 attribute = ["sex", "race", "age"]
-preAlgorithm = ["disparate_impact_remover", "lft", "optim", "reweighing"] 
+preAlgorithm = ["disparate_impact_remover", "lfr", "optim", "reweighing"] 
 inAlgorithm = ["adversarial_debiasing", "art_classifier", "prejudice_remover"]
+#inAlgorithm = ["adversarial_debiasing", "art_classifier"]
 postAlgorithm = ["calibrated_eqodds", "eqodds", "reject_option"]    
-    
+
 # Verify metric name
 allowed_metrics = ["Statistical parity difference",
                    "Average odds difference",
@@ -48,106 +54,112 @@ metric_lb = -0.05
 
 def main():
 
-    #f = open('/home/hliu79/firstProject/result/comb.txt', 'w')    
-    f = open('/Users/huihuiliu/fairness Learning Project/result/comb_algorithm00.txt', 'a')        
-    
-    try:
-        for i in range(3):
-            dataset_used = dataset[i]
-            for j in range(3):
-                if i == 0 and j == 2:
-                    continue
-                if i == 1 and j == 1:
-                    continue
-                if i == 2 and j == 2:
-                    continue
-                if j == 0:
-                    arr = dataset_used + " " + "[1, 0, 0]"
-                if j == 1:
-                    arr = dataset_used + " " + "[0, 1, 0]"
-                if j == 2:
-                    arr = dataset_used + " " + "[0, 0 ,1]"
-
-                protected_attribute_used = attribute[j]
-                dataset_original, privileged_groups, unprivileged_groups, optim_options = LoadData(dataset_used, protected_attribute_used)                
-                for l in range(5):
-                    for m in range(4):
-                        for n in range(4):
-                            algorithm_used = arr + " " + "[" + " " + str(l) + " " + str(m) + " " + str(n) + "]"
-                            feature = comb_algorithm(l, m, n, dataset_original, privileged_groups, unprivileged_groups, optim_options)
-                            result = str(algorithm_used + feature)
-                            print(result)
+    #f = open('/home/hliu79/Fairness1/result/comb_transf_test.txt', 'w')    
+    # f = open('/Users/huihuiliu/fairness Learning Project/result/comb_algorithm00.txt', 'a')        
+    #f.close()
+    for i in range(2):
+        dataset_used = dataset[i]
+        for j in range(3):
+            if i == 0 and j == 2:
+                continue
+            if i == 1 and j == 1:
+                continue
+            if i == 2 and j == 2:
+                continue
+            if j == 0:
+                arr = dataset_used + " " + "[1, 0, 0]"
+            if j == 1:
+                arr = dataset_used + " " + "[0, 1, 0]"
+            if j == 2:
+                arr = dataset_used + " " + "[0, 0 ,1]"
+                
+            protected_attribute_used = attribute[j]
+            for l in range(5):
+                for m in range(3):
+                    for n in range(4):
+                        dataset_orig, privileged_groups, unprivileged_groups, optim_options = LoadData(dataset_used, protected_attribute_used)                
+                        algorithm_used = arr + " " + "[" + " " + str(l) + " " + str(m) + " " + str(n) + "]"
+                        #dataset_original = copy.deepcopy(dataset_orig)
+                        feature = comb_algorithm(l, m, n, dataset_orig, privileged_groups, unprivileged_groups, optim_options)
+                        result = str(algorithm_used + feature)
+                        print(result)
+                        with open('comb_transf_adult.txt', 'a') as f:
                             f.write(result)
                             f.write("\n")
-        f.close()
-    except:   
-        f.close()
-        
+                            f.close()
         
 
-def comb_algorithm(l, m, n, dataset_original, privileged_groups,unprivileged_groups, optim_options):
+def comb_algorithm(l, m, n, dataset_original1, privileged_groups1,unprivileged_groups1, optim_options1):
+    
+    dataset_original2 = copy.deepcopy(dataset_original1)
+    privileged_groups2 = copy.deepcopy(privileged_groups1)
+    unprivileged_groups2 = copy.deepcopy(unprivileged_groups1)
+    optim_options2 = copy.deepcopy(optim_options1)
     
     print(l,m,n)
-    dataset_orig_train, dataset_orig_vt = dataset_original.split([0.7], shuffle=True)
+    dataset_orig_train, dataset_orig_vt = dataset_original2.split([0.7], shuffle=True)
     dataset_orig_valid, dataset_orig_test = dataset_orig_vt.split([0.5], shuffle=True)
          
     if l == 0:
         dataset_transf_train, dataset_transf_valid, dataset_transf_test = dataset_orig_train, dataset_orig_valid, dataset_orig_test
     else:
         pre_used = preAlgorithm[l-1]
-        dataset_transf = Pre(pre_used, dataset_orig_train, dataset_orig_valid, dataset_orig_test, privileged_groups,unprivileged_groups, optim_options)
-        dataset_transf_train, dataset_transf_valid, dataset_transf_test = dataset_transf
-     
-        
-        
-    if m ==0: 
-        dataset_orig_valid_pred, dataset_orig_test_pred = train(dataset_transf_train, dataset_transf_valid, dataset_transf_test, privileged_groups, unprivileged_groups)    
+        dataset_transf_train, dataset_transf_valid, dataset_transf_test = Pre(pre_used, dataset_orig_train, dataset_orig_valid, dataset_orig_test, privileged_groups2,unprivileged_groups2, optim_options2)     
+    
+    #assert (l,m,n)!=(2,0,0)
+    #assert not np.all(dataset_transf_train.labels.flatten()==1.0)
+    
+    if m == 0: 
+        dataset_transf_valid_pred, dataset_transf_test_pred = train(dataset_transf_train, 
+                                                                    dataset_transf_valid, dataset_transf_test, privileged_groups2, unprivileged_groups2)    
     else:        
         in_used = inAlgorithm[m-1]
-        for key,value in privileged_groups[0].items():
-                    sens_attr = key
         if in_used == "adversarial_debiasing":
-            dataset_orig_valid_pred, dataset_orig_test_pred = adversarial_debiasing(dataset_transf_train, dataset_transf_valid, dataset_transf_test, privileged_groups, unprivileged_groups, True)
+            dataset_transf_valid_pred, dataset_transf_test_pred = adversarial_debiasing(dataset_transf_train, 
+                                                                                        dataset_transf_valid, dataset_transf_test, privileged_groups2, unprivileged_groups2, True)
         elif in_used == "art_classifier":
-            dataset_orig_valid_pred, dataset_orig_test_pred = art_classifier(dataset_transf_train, dataset_transf_valid, dataset_transf_test, privileged_groups, unprivileged_groups)                  
+            dataset_transf_valid_pred, dataset_transf_test_pred = art_classifier(dataset_transf_train, dataset_transf_valid, dataset_transf_test, privileged_groups2, unprivileged_groups2)                  
         elif in_used == "prejudice_remover":
-            dataset_orig_valid_pred, dataset_orig_test_pred = prejudice_remover(dataset_transf_train, dataset_transf_valid, dataset_transf_test, privileged_groups, unprivileged_groups, sens_attr, 25.0)
-
-
+            for key,value in privileged_groups2[0].items():
+                sens_attr = key
+            dataset_transf_valid_pred, dataset_transf_test_pred = prejudice_remover(dataset_transf_train, dataset_transf_valid, dataset_transf_test, privileged_groups2, unprivileged_groups2, sens_attr, 25.0)
 
     if n == 0:
-        dataset_transf_test_pred = dataset_orig_test_pred
+        dataset_transf_test_pred_transf = dataset_transf_test_pred
         
     else:
         post_used = postAlgorithm[n-1]
         if post_used == "calibrated_eqodds":
-            cpp = CalibratedEqOddsPostprocessing(privileged_groups = privileged_groups,
-                                     unprivileged_groups = unprivileged_groups,
+            cpp = CalibratedEqOddsPostprocessing(privileged_groups = privileged_groups2,
+                                     unprivileged_groups = unprivileged_groups2,
                                      cost_constraint=cost_constraint,
                                      seed=randseed)
-            cpp = cpp.fit(dataset_orig_valid, dataset_orig_valid_pred)
-            dataset_transf_test_pred = cpp.predict(dataset_orig_test_pred)
+            cpp = cpp.fit(dataset_transf_valid, dataset_transf_valid_pred)
+            dataset_transf_test_pred_transf = cpp.predict(dataset_transf_test_pred)
                   
         elif post_used == "eqodds":
-            EO = EqOddsPostprocessing(unprivileged_groups=unprivileged_groups, 
-                                      privileged_groups=privileged_groups, seed = randseed)
-            EO = EO.fit(dataset_orig_valid, dataset_orig_valid_pred)
-            dataset_transf_test_pred = EO.predict(dataset_orig_test_pred)
+            EO = EqOddsPostprocessing(unprivileged_groups=unprivileged_groups2, 
+                                      privileged_groups=privileged_groups2, seed = randseed)
+            EO = EO.fit(dataset_transf_valid, dataset_transf_valid_pred)
+            dataset_transf_test_pred_transf = EO.predict(dataset_transf_test_pred)
                     
         elif post_used == "reject_option":
-            ROC = RejectOptionClassification(unprivileged_groups=unprivileged_groups, 
-                                 privileged_groups=privileged_groups, 
+            ROC = RejectOptionClassification(unprivileged_groups=unprivileged_groups2, 
+                                 privileged_groups=privileged_groups2, 
                                  low_class_thresh=0.01, high_class_thresh=0.99,
                                   num_class_thresh=100, num_ROC_margin=50,
                                   metric_name=allowed_metrics[0],
                                   metric_ub=metric_ub, metric_lb=metric_lb)
-            ROC = ROC.fit(dataset_orig_valid, dataset_orig_valid_pred)
-            dataset_transf_test_pred = ROC.predict(dataset_orig_test_pred)
-
+            ROC = ROC.fit(dataset_transf_valid, dataset_transf_valid_pred)
+            dataset_transf_test_pred_transf = ROC.predict(dataset_transf_test_pred)
+            
+    #print(dataset_transf_test.features)
+    #print(dataset_transf_test_pred.features)
+    
     metric = ClassificationMetric(
-                dataset_orig_test, dataset_transf_test_pred,
-                unprivileged_groups=unprivileged_groups,
-                privileged_groups=privileged_groups)
+                dataset_transf_test, dataset_transf_test_pred_transf,
+                unprivileged_groups=unprivileged_groups2,
+                privileged_groups=privileged_groups2)
     
     metrics = OrderedDict()
     metrics["Classification accuracy"] = metric.accuracy()
@@ -161,7 +173,7 @@ def comb_algorithm(l, m, n, dataset_original, privileged_groups,unprivileged_gro
     metrics["Average odds difference"] = metric.average_odds_difference()
     metrics["Theil index"] = metric.theil_index()
     metrics["United Fairness"] = metric.generalized_entropy_index()
-    print(metrics)
+    # print(metrics)
 
     
     feature = "["
@@ -170,6 +182,6 @@ def comb_algorithm(l, m, n, dataset_original, privileged_groups,unprivileged_gro
     feature = feature + "]"
     
     return feature
-        
-        
+
+
 main()        
