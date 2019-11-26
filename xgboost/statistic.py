3aa19b57e622f7ea0c7846f513723b5c38b13e3b
ihollywhy <yangyd92@gmail.com>
2019-11-25 19:42:43 -0500

    pair done

--- xgboost/statistic.py
+++ xgboost/statistic.py
@@ -41,22 +41,48 @@ def pairCompasion(records, keys, method1, method2):
         t_method1 = target_v[row1]
         t_method2 = target_v[row2]
 
-        plt.plot(t_method1)
-        plt.plot(t_method2)
-        plt.show()
-
+        hist1, bins1 = np.histogram(t_method1, bins=20, range=(-3,3))
+        hist2, bins2 = np.histogram(t_method2, bins=20, range=(-3,3))
+        
+        print(target, method1, method2)
+        plt.plot(bins1[:-1], hist1)
+        plt.plot(bins2[:-1], hist2)
+        plt.legend([method1, method2])
+        #plt.show()
+        plt.savefig(f"/mnt/svm/code/Fairness/vis/pair_noconstraint/{target}_{method1}_{method2}.png")
+        plt.close()
     return
 
-def compareAll(records, keys):
+def compareAllPairs(records, keys):
     preProcesses = ["Nothing","DisparateImpactRemover","LFR","OptimPreproc","Reweighing"]
     inProcesses = ["PlainModel","AdversarialDebiasing","ARTClassifier","PrejudiceRemover"]
     postProcesses = ["Nothing","CalibratedEqOddsPostprocessing","EqOddsPostprocessing","RejectOptionClassification"]
     for methods in [preProcesses, inProcesses, postProcesses]:
         for i in range(len(methods)):
-            for j in range(i, len(methods)):
+            for j in range(i+1, len(methods)):
                 pairCompasion(records, keys, methods[i], methods[j])      
+    return
+
+def compareAll(records, keys):
+    for target in ['TPR','TNR','FPR','FNR','Balanced_Acc','Acc',
+            "Statistical parity difference","Disparate impact","Equal opportunity difference",
+            "Average odds difference","Theil index","United Fairness"]:
+        methodsAll = {}
+        target_index = keys.index(target)
+            
+        for record in records:
+            methodsComb = str(record[:13])
+            methodsAll[methodsComb] = methodsAll.get(methodsComb, []) + [record[target_index]]
+
+        for combRes in methodsAll.values():
+            hist, bins = np.histogram(combRes, bins=40, range=(-2,2))
+        
+            plt.plot(bins[:-1], hist)
+        print(len(list(methodsAll.keys())))
+        plt.show()
     return
 
 if __name__ == "__main__":
     records, keys = getData()
+    # compareAllPairs(records, keys)
     compareAll(records, keys)
