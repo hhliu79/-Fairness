18e89f473764d3fe6c8cdff941e1f97f2f0d2ebe
ihollywhy <yangyd92@gmail.com>
2019-12-02 13:25:49 -0500

    plot all done

--- xgboost/statistic.py
+++ xgboost/statistic.py
@@ -63,23 +63,57 @@ def compareAllPairs(records, keys):
                 pairCompasion(records, keys, methods[i], methods[j])      
     return
 
+class drawCircles():
+    def __init__(self):
+        ax = plt.gca()
+        ax.set_aspect(0.6)
+        #fig, ax = plt.subplots()
+        ax.set_xlim((-10,1000))
+        ax.set_ylim((0,810*3))
+        ax.set_axis_off()
+        self.ax = ax
+
+    def addCircles(self, xs, ys, ds):
+        for x, y, d in zip(xs, ys, ds):
+            self.addCircle(x, y, d)
+
+    def addCircle(self, x, y, d):
+        x, y, d = int(x), int(y), int(d)
+        circle = plt.Circle((x, y), d, color='k', fill=True)
+        self.ax.add_artist(circle)
+    
+    def show(self):
+        plt.show()
+
+    def save(self, saveto):
+        plt.savefig(saveto, dpi=300)
+        plt.close()
+
+
 def compareAll(records, keys):
     for target in ['TPR','TNR','FPR','FNR','Balanced_Acc','Acc',
             "Statistical parity difference","Disparate impact","Equal opportunity difference",
             "Average odds difference","Theil index","United Fairness"]:
         methodsAll = {}
         target_index = keys.index(target)
-            
+        
+        ins = drawCircles()
+
         for record in records:
             methodsComb = str(record[:13])
             methodsAll[methodsComb] = methodsAll.get(methodsComb, []) + [record[target_index]]
 
-        for combRes in methodsAll.values():
-            hist, bins = np.histogram(combRes, bins=40, range=(-2,2))
-        
-            plt.plot(bins[:-1], hist)
+        for index, combRes in enumerate(methodsAll.values()):
+            minv = np.min(combRes)
+            maxv = np.max(combRes)
+            hist, bins = np.histogram(combRes, bins=80, range=(minv,maxv))
+            bins = bins[:-1]
+            bins = (bins - minv) * (1000/(maxv - minv))
+            hist = (hist*6)/np.max(hist)
+            ins.addCircles(bins, np.array([(index+1)*30]*80), hist)
         print(len(list(methodsAll.keys())))
-        plt.show()
+        saveto = f"/mnt/svm/code/Fairness/vis/all/{target}.png"
+        ins.save(saveto)
     return
 
 if __name__ == "__main__":
