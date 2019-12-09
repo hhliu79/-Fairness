d6175e2c795dc1e217ee994f08f6d6b6a6548f8a
ihollywhy <yangyd92@gmail.com>
2019-12-09 12:14:15 -0500

    ranking done

--- xgboost/statistic.py
+++ xgboost/statistic.py
@@ -3,18 +3,24 @@ import numpy as np
 from data_utils import load_txt_haipei, listDir
 import os
 import pickle
+import tqdm
 
-
-def getData():
-    dirName = "/mnt/svm/code/Fairness/Haipei"
-    
-    if os.path.isfile(os.path.join(dirName, "records.pkl")):
-        with open( os.path.join(dirName, "records.pkl"), 'rb' ) as f:
+def getData(fileName=None):
+    if fileName:
+        with open( fileName, 'rb' ) as f:
             return pickle.load(f)
+   
+    dirName = "/mnt/svm/code/Fairness/Haipei"
+ 
     fileList = listDir(dirName, ends=".txt")
     records = None
     for fileName in fileList:
         instance, keys= load_txt_haipei(fileName)
+        saveto = fileName.replace('.txt', '.pkl')
+        assert saveto!=fileName
+        with open(saveto, 'wb') as f:
+            pickle.dump((instance, keys), f)
+
         if records is None:
             records = instance
         else:
@@ -120,37 +126,135 @@ def ranking(values):
     rank = np.argsort(values)
     return rank
 
-def aggregateRankings(ranks):
-    return 
+def aggregateRankings(loadfrom=None):
+    targets = ['Balanced_Acc','Acc', "Statistical parity difference","Disparate impact",
+                "Equal opportunity difference","Average odds difference","Theil index","United Fairness"]
+    targets_showName = ['BAcc','Acc', "SPD","DI",
+                "EOD","AOD","TI","UF"]
 
-def rankingAll(records, keys):
-    ranks = []
-    saveto = f'/mnt/svm/code/Fairness/vis/ranks/ranks.pkl'
+    if loadfrom is None:
+        loadfrom = f'/mnt/svm/code/Fairness/vis/ranks/ranks.pkl'
+    with open(loadfrom, 'rb') as f:
+        methodsAllTarget = pickle.load(f)
+
+    ranksTargetsMethods = {}
+    allComb = []
+    # target->method->rank
+    for target in methodsAllTarget.keys():
+        ranksPerTarget = {}
+        methodsPerTarget = methodsAllTarget[target]
+        methods= []
+        performs = []
+        for method in methodsPerTarget.keys():
+            meanPerformance = np.mean(methodsPerTarget[method])
+            methods.append(method)
+            performs.append(meanPerformance)
+        ranks = ranking(performs)
+        allComb = methods
+        for method, rank in zip(methods, ranks):
+            ranksPerTarget[method] = rank
+        
+        ranksTargetsMethods[target] = ranksPerTarget
+
+    # for combIndex, comb in enumerate(sorted(allComb)):
+    #     rankAllTargets = []
+    #     for target in targets:
+    #         rankAllTargets.append(ranksTargetsMethods[target][comb])
+        
+    #     plt.plot(np.array(rankAllTargets))
+    #     plt.xticks(list(range(8)), targets_showName)
+    #     title = 'dataset: ' + os.path.basename(loadfrom) + '\n' + 'methods: ' + comb + '\n' + f'methodsIndex: {combIndex}'
+    #     saveName = 'dataset:' + os.path.basename(loadfrom) + 'methods:' + comb + f'methodsIndex:{combIndex}'
+    #     saveName = "/mnt/svm/code/Fairness/vis/ranks/perMethodperDataset/"+saveName+'.png'
+        
+    #     plt.title(title)
+    #     plt.ylabel('ranking')
+    #     plt.xlabel('criteria')
+    #     plt.subplots_adjust(top=0.83)
+    #     plt.savefig(saveName)
+    #     plt.close()
+    
+    # for target in targets:
+    #     methodsPerTarget = []
+    #     for combIndex, comb in enumerate(sorted(allComb)):
+    #         methodsPerTarget.append(ranksTargetsMethods[target][comb])
+        
+    #     plt.bar(list(range(80)), np.array(methodsPerTarget))
+    #     # plt.xticks(list(range(80)), list(range(80)))
+    #     title = 'dataset: ' + os.path.basename(loadfrom) + '\n' + 'target: ' + target
+    #     saveName = 'dataset:' + os.path.basename(loadfrom) + 'target:' + target
+    #     saveName = "/mnt/svm/code/Fairness/vis/ranks/methodsPerTarget/"+saveName+'.png'
+        
+    #     plt.title(title)
+    #     plt.ylabel('ranking')
+    #     plt.xlabel('comb method index')
+    #     plt.subplots_adjust(top=0.83)
+    #     # plt.show()
+    #     plt.savefig(saveName)
+    #     plt.close()
+    
+    totalRanking = []
+    for combIndex, comb in enumerate(sorted(allComb)):
+        perMethod = 0
+        for target in targets:
+            perMethod += ranksTargetsMethods[target][comb]
+        totalRanking.append(perMethod)
+    
+    plt.bar(list(range(80)), np.array(totalRanking))
+    title = 'dataset: ' + os.path.basename(loadfrom)
+    saveName = 'dataset:' + os.path.basename(loadfrom)
+    saveName = "/mnt/svm/code/Fairness/vis/ranks/totalRankingPerDataset/"+saveName+'.png'
+    
+    plt.title(title)
+    plt.ylabel('ranking')
+    plt.xlabel('comb method index')
+    plt.subplots_adjust(top=0.83)
+    # plt.show()
+    plt.savefig(saveName)
+    plt.close()
+
+
+def rankingAll(records, keys, fileName=None):
+    methodsAllTarget = {}
+    if fileName:
+        saveto = fileName.replace('.pkl', '_ranks.pkl')
+        assert saveto != fileName
+    else:
+        saveto = f'/mnt/svm/code/Fairness/vis/ranks/ranks.pkl'
     # 'TPR','TNR','FPR','FNR',
     targets = ['Balanced_Acc','Acc', "Statistical parity difference","Disparate impact",
                 "Equal opportunity difference","Average odds difference","Theil index","United Fairness"]
     bests = [1, 1, 0, 1, 0, 0, 0, 0]
-    for target, best in zip(targets, bests):
-        methodsAll = {}
+    for target, best in tqdm.tqdm(zip(targets, bests)):
+        methodsPerTarget = {}
         target_index = keys.index(target)
     
+        # compute all record according to the target and best
         for record in records:
             # for single record
             methodsComb = str(record[:13])
             diffFromBest = np.abs(record[target_index] - best) # smaller is better
-            methodsAll[methodsComb] = methodsAll.get(methodsComb, []) + [diffFromBest]
+            methodsPerTarget[methodsComb] = methodsPerTarget.get(methodsComb, []) + [diffFromBest]
         
-        combResMean = [np.mean(combRes) for index, combRes in enumerate(methodsAll.values())]
-        rank = ranking(combResMean)
-        ranks.append(rank)
-    
-    with open(saveto, 'rb') as f:
-        pickle.dump(ranks, f)
-    return ranks
+        methodsAllTarget[target] = methodsPerTarget
+
+    with open(saveto, 'wb') as f:
+        pickle.dump(methodsAllTarget, f)
+    return methodsAllTarget
 
 
 if __name__ == "__main__":
-    records, keys = getData()
+    dirName = '/mnt/svm/code/Fairness/Haipei'
+    fileList = listDir(dirName, ends=".pkl")
+    for fileName in fileList:
+        if '_ranks.pkl' in fileName:
+            continue
+        # records, keys = getData(fileName)
+        # rankingAll(records, keys, fileName=fileName)
+        
+        loadfrom = fileName.replace('.pkl', '_ranks.pkl')
+        aggregateRankings(loadfrom)
+    
     # compareAllPairs(records, keys)
     # compareAll(records, keys)
-    rankingAll(records, keys)
+    
